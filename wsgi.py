266ebbd1ede1f46e2f459d752f0a59030dc79ecf
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2025-01-06 20:03:19 +0000

    Day 55 progress

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 import requests
 app = Flask(__name__)
@@ -6,24 +5,42 @@ app = Flask(__name__)
 
 requests.get("https://www.google.com")
 
+
+def make_bold(function):
+  def wrapper_function():
+    return f'<b>{function()}<b>'
+  return wrapper_function
+
+def make_emphasis(function):
+  def wrapper_function():
+    return f'<em>{function()}<em>'
+  return wrapper_function
+
+def make_underlined(function):
+  def wrapper_function():
+    return f'<u>{function()}<u>'
+  return wrapper_function
+
+
 @app.route('/')
 def hello_world():
   return '<h1 style="text-align: center">Hello World!</h1>\
           <p>This is a paragraph.</p>\
-          <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcatastic.pet%2Fcat-breeds%2F11-interesting-facts-about-the-munchkin-cat%2F&psig=AOvVaw2zuerc9eIHZpah7lS5EkSs&ust=1736193512154000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCjsISv34oDFQAAAAAdAAAAABAp">'
+          <img src="https://api.deepai.org/job-view-file/5d3fa914-ce43-45cc-afe1-8bcc640a68fa/outputs/output.jpg" width=200>\
+          <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmxkZWlxem51bmFuMDdib2c0NTF4dnloNmQwcmRsYW4xcWZsZXgxZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KYgNd2BvEuRfZjIZRs/giphy.webp">'
+
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
-  return "Bye"
+  return "<u><em>Bye</em></u>"
 
 
 @app.route("/username/<name>/<int:number>")
 def greet(name):
   return f"Hello there {name + '12'}!"
-
-
-
-
 
 
 
