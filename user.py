b00c01b31f0c1e60803b7fdad12e4c9692b03d23
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2025-01-06 20:03:45 +0000

    Day 55 progress

---
+++ user.py
@@ -0,0 +1,19 @@
+class user:
+  def __init__(self, username):
+    self.user = username
+    self.is_online = False
+
+
+def authenticated_decorator(function):
+  def wrapper_function(*args, **kwargs):
+    if args[0].is_online == True:
+      return f'{function(args[0].user)} is authenticated'
+
+@authenticated_decorator
+def is_authenticated(function):
+  return print(f'This is {function.user}`s blogpost')
+
+
+user_object = user('Szymon')
+user_object.is_online = True
+is_authenticated(user_object)
