c1de474872b920e6cfcd3fd62431afe61adcd2d8
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2025-01-11 17:35:35 +0000

    Day 55 progress

--- app.py
+++ app.py
@@ -15,9 +15,16 @@ def display_h1():
 def random_numbers(number):
   random_number = random.randint(0, 9)
   if random_number == number:
-    return '<h1>you guessed the number</h1>'
-  else:
-    return "<h1>you didn't guess the number</h1>"
+    return f'<h1>your number was: {number} you guessed the number.</h1>'
+  elif random_number > number:
+    return 'Too low, try again!\
+              <br> \
+              <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYW9uNmVrbnNxMXZ4N3E5ZXRtM2pqMDcyYXdsZDNoZThyc3RzNzdteiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2kS8TzTRJz8hFhLHMK/giphy.webp">'
+
+  elif random_number < number:
+    return 'Too high, try again! \
+      <br> \
+      <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2Roc3RzOHJpZTM2d2FiNTBrdzdqODYwNXhwbWFrOHhxajA2NHh5eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6ZtaP9whaYPIK00g/giphy.webp">'
   
  
 
